0cbfc761df0ccab79eaadb7124c333d69610ca71
Muhammed Çelik <celikmuhammed21@hotmail.com>
2019-08-04 05:12:56 +0300

    Add project files.

---
+++ 04_Views/Controllers/Home2Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_Views.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult contact1()
+        {
+            return View();
+        }
+
+    }
+    
+}
